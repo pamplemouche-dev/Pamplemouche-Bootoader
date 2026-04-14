c899753ed3a052dacba25e28db0a3c2107546d9f
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-04-14 18:37:42 +0200

    Update symbols.c

--- src/symbols.c
+++ src/symbols.c
@@ -1,14 +1,10 @@
-#include "efi.h"
-#include "bootargs.h"
+#include <stdint.h>
+#include <stddef.h>
 
-void SetupArchitecture(boot_args *args) {
-    // Sur Ivy Bridge, on doit s'assurer que le bit NX (No-Execute) est géré
-    // et que les registres du processeur sont prêts pour le 64-bit d'Apple.
-    
-    args->Revision = 0x01;
-    args->Version = 0x02;
-    
-    // On définit ici l'identité du Mac (SMBIOS)
-    // Pour un C850, on simule un MacBookPro9,2 (Ivy Bridge)
-    // Cela permet à macOS Tahoe d'activer les bons drivers graphiques HD 4000.
+// Cette fonction permet d'initialiser la mémoire manuellement
+void *memset(void *s, int c, size_t n) {
+    unsigned char *p = s;
+    while (n--)
+        *p++ = (unsigned char)c;
+    return s;
 }
