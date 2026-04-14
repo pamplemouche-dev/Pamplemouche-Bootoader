a88d6ec9e8f860fc75f8b6a540e69a02c9f05688
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-04-14 18:26:41 +0200

    Update efi.h

--- include/efi.h
+++ include/efi.h
@@ -12,21 +12,24 @@ typedef uint16_t CHAR16;
 
 #define EFI_SUCCESS 0
 
-typedef struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
-    void* Reset;
-    EFI_STATUS (EFIAPI *OutputString)(struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, const CHAR16 *String);
-} EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
+// --- STRUCTURES GRAPHIQUES (GOP) ---
+typedef struct {
+    uint32_t Version;
+    uint32_t HorizontalResolution;
+    uint32_t VerticalResolution;
+    int      PixelFormat;
+    uint32_t PixelInformation[4];
+    uint32_t PixelsPerScanLine;
+} EFI_GRAPHICS_OUTPUT_MODE_INFORMATION;
 
 typedef struct {
-    uint16_t ScanCode;
-    CHAR16   UnicodeChar;
-} EFI_INPUT_KEY;
-
-typedef struct _EFI_SIMPLE_TEXT_INPUT_PROTOCOL {
-    void* Reset;
-    EFI_STATUS (EFIAPI *ReadKeyStroke)(struct _EFI_SIMPLE_TEXT_INPUT_PROTOCOL *This, EFI_INPUT_KEY *Key);
-    void* WaitForKey;
-} EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
+    uint32_t MaxMode;
+    uint32_t Mode;
+    EFI_GRAPHICS_OUTPUT_MODE_INFORMATION *Info;
+    uint64_t Size;
+    uint64_t FrameBufferBase;
+    uint64_t FrameBufferSize;
+} EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE;
 
 typedef struct {
     uint32_t Blue;
@@ -35,15 +38,16 @@ typedef struct {
     uint32_t Reserved;
 } EFI_GRAPHICS_OUTPUT_BLT_PIXEL;
 
-typedef enum { EfiBltVideoFill, EfiBltVideoToBltBuffer, EfiBltBufferToVideo, EfiBltVideoToVideo, EfiGraphicsOutputBltMax } EFI_GRAPHICS_OUTPUT_BLT_OPERATION;
+typedef enum { EfiBltVideoFill, EfiGraphicsOutputBltMax } EFI_GRAPHICS_OUTPUT_BLT_OPERATION;
 
 typedef struct _EFI_GRAPHICS_OUTPUT_PROTOCOL {
     void* QueryMode;
     void* SetMode;
     EFI_STATUS (EFIAPI *Blt)(struct _EFI_GRAPHICS_OUTPUT_PROTOCOL *This, EFI_GRAPHICS_OUTPUT_BLT_PIXEL *BltBuffer, EFI_GRAPHICS_OUTPUT_BLT_OPERATION BltOperation, uint64_t SourceX, uint64_t SourceY, uint64_t DestinationX, uint64_t DestinationY, uint64_t Width, uint64_t Height, uint64_t Delta);
-    struct { uint32_t MaxMode; uint32_t Mode; void* Info; uint64_t Size; uint64_t FrameBufferBase; uint64_t FrameBufferSize; } *Mode;
+    EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE *Mode;
 } EFI_GRAPHICS_OUTPUT_PROTOCOL;
 
+// --- SERVICES DE BOOT ---
 typedef struct {
     char Signature[8];
     uint32_t Revision;
@@ -51,18 +55,20 @@ typedef struct {
     uint32_t CRC32;
     uint32_t Reserved;
     void* _pad[2]; 
-    EFI_STATUS (EFIAPI *AllocatePages)(uint32_t Type, uint32_t MemoryType, uint64_t Pages, void **Memory);
+    void* AllocatePages;
     void* FreePages;
-    EFI_STATUS (EFIAPI *GetMemoryMap)(uint64_t *MemoryMapSize, void *MemoryMap, uint64_t *MapKey, uint64_t *DescriptorSize, uint32_t *DescriptorVersion);
-    void* _pad2[2];
-    void* _pad3[5];
+    void* GetMemoryMap;
+    void* _pad2[9];
     EFI_STATUS (EFIAPI *WaitForEvent)(uint32_t NumberOfEvents, void **Event, uint64_t *Index);
-    void* _pad4[8];
-    void* _pad5[4];
-    EFI_STATUS (EFIAPI *ExitBootServices)(EFI_HANDLE ImageHandle, uint64_t MapKey);
-    void* _pad6[10];
+    void* _pad3[13];
     EFI_STATUS (EFIAPI *LocateProtocol)(void *Protocol, void *Registration, void **Interface);
 } EFI_BOOT_SERVICES;
+
+// --- TABLE SYSTÈME ---
+typedef struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
+    void* Reset;
+    EFI_STATUS (EFIAPI *OutputString)(struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, const CHAR16 *String);
+} EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
 typedef struct {
     char Signature[8];
@@ -71,7 +77,7 @@ typedef struct {
     uint32_t CRC32;
     uint32_t Reserved;
     EFI_HANDLE ConsoleInHandle;
-    EFI_SIMPLE_TEXT_INPUT_PROTOCOL *ConIn;
+    void *ConIn;
     EFI_HANDLE ConsoleOutHandle;
     EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut;
     void* RuntimeServices;
