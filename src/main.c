c4afb41cb55f94309027caa6e821f8dc2a0b69ea
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-04-14 18:40:54 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,24 +1,19 @@
 #include "efi.h"
 
-// Déclarations
 extern void SetDeepBlack(EFI_SYSTEM_TABLE *ST);
 extern void PatchHardware(EFI_SYSTEM_TABLE *ST);
 
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *ST) {
-    // 1. Graphismes
+    if (!ST || !ST->ConOut) return 1;
+
     SetDeepBlack(ST);
-    
-    // 2. Patch Ivy Bridge
-    PatchHardware(ST);
 
-    // 3. Texte Flow
     ST->ConOut->OutputString(ST->ConOut, L"RADICAL BOOT - FLOW UI\r\n");
-    ST->ConOut->OutputString(ST->ConOut, L"Ready for macOS Tahoe Recovery\r\n\r\n");
-    ST->ConOut->OutputString(ST->ConOut, L"Press any key to boot...\r\n");
+    ST->ConOut->OutputString(ST->ConOut, L"Ready for macOS Tahoe Recovery\r\n");
 
-    // Attente
+    // Attente simple
     uint64_t index;
-    ST->BootServices->WaitForEvent(1, &ST->ConIn->WaitForKey, &index);
-    
+    ST->BootServices->WaitForEvent(1, &ST->ConIn, &index);
+
     return EFI_SUCCESS;
 }
