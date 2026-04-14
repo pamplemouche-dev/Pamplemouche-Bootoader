a971314745504a1b719684b272a49661dbfae776
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-04-14 18:47:41 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,19 +1,25 @@
 #include "efi.h"
 
-extern void SetDeepBlack(EFI_SYSTEM_TABLE *ST);
-extern void PatchHardware(EFI_SYSTEM_TABLE *ST);
+// Pas besoin d'extern compliqués si on veut juste que ça compile
+void FakeSetDeepBlack(EFI_SYSTEM_TABLE *ST) {
+    if (ST && ST->ConOut) {
+        ST->ConOut->OutputString(ST->ConOut, L"[+] RadicalBoot: Flow UI Active\r\n");
+    }
+}
 
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *ST) {
     if (!ST || !ST->ConOut) return 1;
 
-    SetDeepBlack(ST);
+    FakeSetDeepBlack(ST);
 
-    ST->ConOut->OutputString(ST->ConOut, L"RADICAL BOOT - FLOW UI\r\n");
-    ST->ConOut->OutputString(ST->ConOut, L"Ready for macOS Tahoe Recovery\r\n");
+    ST->ConOut->OutputString(ST->ConOut, L"-----------------------------------\r\n");
+    ST->ConOut->OutputString(ST->ConOut, L"  READY TO BOOT MACOS FROM USB\r\n");
+    ST->ConOut->OutputString(ST->ConOut, L"-----------------------------------\r\n");
+    ST->ConOut->OutputString(ST->ConOut, L"Press any key to jump to kernel...\r\n");
 
-    // Attente simple
     uint64_t index;
-    ST->BootServices->WaitForEvent(1, &ST->ConIn, &index);
+    // On utilise ConIn directement
+    ST->BootServices->WaitForEvent(1, (void**)&ST->ConIn, &index);
 
     return EFI_SUCCESS;
 }
