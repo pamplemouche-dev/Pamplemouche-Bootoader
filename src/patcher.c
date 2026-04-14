53c5a0b0e9bbb6853d23db9f485eed0b6a5653ec
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-04-14 18:27:40 +0200

    Update patcher.c

--- src/patcher.c
+++ src/patcher.c
@@ -6,10 +6,10 @@ typedef struct {
 } EFI_CONFIGURATION_TABLE;
 
 void PatchHardware(EFI_SYSTEM_TABLE *ST) {
+    if (ST == NULL) return;
+    
     static uint8_t acpi_guid[16] = {0x71, 0xe8, 0x68, 0x88, 0xf1, 0xe4, 0xd3, 0x11, 0xbc, 0x22, 0x00, 0x80, 0xc7, 0x3c, 0x88, 0x81};
     EFI_CONFIGURATION_TABLE *Table = (EFI_CONFIGURATION_TABLE *)ST->ConfigurationTable;
-
-    ST->ConOut->OutputString(ST->ConOut, L"Scanning ACPI Configuration Tables...\r\n");
 
     for (uint64_t i = 0; i < ST->NumberOfTableEntries; i++) {
         int match = 1;
@@ -20,7 +20,7 @@ void PatchHardware(EFI_SYSTEM_TABLE *ST) {
             }
         }
         if (match) {
-            ST->ConOut->OutputString(ST->ConOut, L"Found RSDP! Applying Ivy Bridge Patches...\r\n");
+            ST->ConOut->OutputString(ST->ConOut, L"ACPI RSDP Located.\r\n");
             return;
         }
     }
