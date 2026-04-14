8a522af263e897c04722c6deddbfa5d3d63aa7e9
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-04-14 18:04:57 +0200

    Update dmg.h

--- include/dmg.h
+++ include/dmg.h
@@ -1,19 +1,44 @@
 #ifndef DMG_H
 #define DMG_H
 
-#include "efi.h"
+#include <stdint.h>
 
-// macOS utilise souvent le format HFS+ ou APFS dans les DMG.
-// On définit ici la signature pour localiser le début de l'image.
-#define DMG_SIG 0x6b6f6c79 // 'koly' en ASCII
+// Signature "koly" (0x6b6f6c79) en Big Endian
+#define DMG_KOLY_SIGNATURE 0x6b6f6c79
 
+#pragma pack(push, 1) // Force l'alignement strict pour la lecture binaire
 typedef struct {
-    char     Signature[4];
-    uint32_t Version;
-    uint32_t HeaderSize;
-    uint64_t DataForkOffset;
-    uint64_t DataForkLength;
-    // ... autres champs du header DMG
-} dmg_header;
+    uint32_t Signature;          // 'koly'
+    uint32_t Version;            // Actuellement 4
+    uint32_t HeaderSize;         // Taille de cette structure (120 octets)
+    uint32_t Flags;
+    uint64_t RunningDataForkOffset;
+    uint64_t DataForkOffset;     // Début des données réelles
+    uint64_t DataForkLength;     // Taille des données
+    uint64_t RsrcForkOffset;     // Souvent 0
+    uint64_t RsrcForkLength;
+    uint32_t SegmentNumber;
+    uint32_t SegmentCount;
+    uint8_t  SegmentID[16];
+
+    uint32_t DataChecksumType;
+    uint32_t DataChecksumSize;
+    uint32_t DataChecksum[32];
+
+    uint64_t XMLOffset;          // Emplacement de la Plist (Description des blocs)
+    uint64_t XMLLength;          // Taille de la Plist
+    uint8_t  Reserved1[120];     // Espace réservé
+} DMG_KOLY_HEADER;
+#pragma pack(pop)
+
+// Structure pour le "Block Chunk" (Si le DMG est compressé en UDZO)
+typedef struct {
+    uint32_t EntryType;
+    uint32_t Comment;
+    uint64_t SectorOffset;
+    uint64_t SectorCount;
+    uint64_t CompressedOffset;
+    uint64_t CompressedLength;
+} DMG_BLOCK_CHUNK;
 
 #endif
