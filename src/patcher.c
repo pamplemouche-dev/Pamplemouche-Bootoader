3a21994e6a1ef29ce8684ebb62c52906150847df
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-04-14 18:41:10 +0200

    Update patcher.c

--- src/patcher.c
+++ src/patcher.c
@@ -1,24 +1,22 @@
 #include "efi.h"
 
-typedef struct {
-    uint8_t  Guid[16];
-    void    *VendorTable;
-} EFI_CONFIGURATION_TABLE;
-
 void PatchHardware(EFI_SYSTEM_TABLE *ST) {
-    if (ST == NULL) return;
-    
-    static uint8_t acpi_guid[16] = {0x71, 0xe8, 0x68, 0x88, 0xf1, 0xe4, 0xd3, 0x11, 0xbc, 0x22, 0x00, 0x80, 0xc7, 0x3c, 0x88, 0x81};
-    EFI_CONFIGURATION_TABLE *Table = (EFI_CONFIGURATION_TABLE *)ST->ConfigurationTable;
+    if (!ST || !ST->ConfigurationTable) return;
 
+    static uint8_t acpi_guid[16] = {0x71, 0xe8, 0x68, 0x88, 0xf1, 0xe4, 0xd3, 0x11, 0xbc, 0x22, 0x00, 0x80, 0xc7, 0x3c, 0x88, 0x81};
+    
+    // On boucle sur les tables
     for (uint64_t i = 0; i < ST->NumberOfTableEntries; i++) {
+        EFI_CONFIGURATION_TABLE *table = &ST->ConfigurationTable[i];
+        
         int match = 1;
         for (int j = 0; j < 16; j++) {
-            if (((uint8_t*)&Table[i].Guid)[j] != acpi_guid[j]) {
+            if (table->Guid[j] != acpi_guid[j]) {
                 match = 0;
                 break;
             }
         }
+        
         if (match) {
             ST->ConOut->OutputString(ST->ConOut, L"ACPI RSDP Located.\r\n");
             return;
