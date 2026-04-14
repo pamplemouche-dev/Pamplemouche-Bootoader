2250236f6ad87e11d5c2088fd2c2ea220617e814
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-04-14 18:20:09 +0200

    Update efi.h

--- include/efi.h
+++ include/efi.h
@@ -5,8 +5,6 @@
 
 #define EFIAPI __attribute__((ms_abi))
 #define NULL ((void *)0)
-#define FALSE 0
-#define TRUE 1
 
 typedef void* EFI_HANDLE;
 typedef uint64_t EFI_STATUS;
@@ -14,13 +12,11 @@ typedef uint16_t CHAR16;
 
 #define EFI_SUCCESS 0
 
-// Protocole Sortie Texte
 typedef struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
     void* Reset;
     EFI_STATUS (EFIAPI *OutputString)(struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, const CHAR16 *String);
 } EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
-// Protocole Entrée Texte
 typedef struct {
     uint16_t ScanCode;
     CHAR16   UnicodeChar;
@@ -32,7 +28,6 @@ typedef struct _EFI_SIMPLE_TEXT_INPUT_PROTOCOL {
     void* WaitForKey;
 } EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
 
-// Protocole Graphique (GOP)
 typedef struct {
     uint32_t Blue;
     uint32_t Green;
@@ -49,61 +44,26 @@ typedef struct _EFI_GRAPHICS_OUTPUT_PROTOCOL {
     struct { uint32_t MaxMode; uint32_t Mode; void* Info; uint64_t Size; uint64_t FrameBufferBase; uint64_t FrameBufferSize; } *Mode;
 } EFI_GRAPHICS_OUTPUT_PROTOCOL;
 
-// Services de Boot (Complet)
 typedef struct {
     char Signature[8];
     uint32_t Revision;
     uint32_t HeaderSize;
     uint32_t CRC32;
     uint32_t Reserved;
-    
-    void* RaiseTPL;
-    void* RestoreTPL;
-
+    void* _pad[2]; 
     EFI_STATUS (EFIAPI *AllocatePages)(uint32_t Type, uint32_t MemoryType, uint64_t Pages, void **Memory);
     void* FreePages;
     EFI_STATUS (EFIAPI *GetMemoryMap)(uint64_t *MemoryMapSize, void *MemoryMap, uint64_t *MapKey, uint64_t *DescriptorSize, uint32_t *DescriptorVersion);
-    void* AllocatePool;
-    void* FreePool;
-
-    void* CreateEvent;
-    void* SetTimer;
+    void* _pad2[2];
+    void* _pad3[5];
     EFI_STATUS (EFIAPI *WaitForEvent)(uint32_t NumberOfEvents, void **Event, uint64_t *Index);
-    void* SignalEvent;
-    void* CloseEvent;
-    void* CheckEvent;
-
-    void* InstallProtocolInterface;
-    void* ReinstallProtocolInterface;
-    void* UninstallProtocolInterface;
-    void* HandleProtocol;
-    void* PHandleProtocol;
-    void* RegisterProtocolNotify;
-    void* LocateHandle;
-    void* LocateDevicePath;
-    void* InstallConfigurationTable;
-
-    void* LoadImage;
-    void* StartImage;
-    void* Exit;
-    void* UnloadImage;
+    void* _pad4[8];
+    void* _pad5[4];
     EFI_STATUS (EFIAPI *ExitBootServices)(EFI_HANDLE ImageHandle, uint64_t MapKey);
-
-    void* GetNextMonotonicCount;
-    void* Stall;
-    void* SetWatchdogTimer;
-
-    void* ConnectController;
-    void* DisconnectController;
-    void* OpenProtocol;
-    void* CloseProtocol;
-    void* OpenProtocolInformation;
-    void* ProtocolsPerHandle;
-    void* LocateHandleBuffer;
+    void* _pad6[10];
     EFI_STATUS (EFIAPI *LocateProtocol)(void *Protocol, void *Registration, void **Interface);
 } EFI_BOOT_SERVICES;
 
-// Table Système
 typedef struct {
     char Signature[8];
     uint32_t Revision;
