793521f469cf3e4cdce38b46f917870239ad6c25
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-04-14 18:47:21 +0200

    Update efi.h

--- include/efi.h
+++ include/efi.h
@@ -5,74 +5,37 @@
 #include <stddef.h>
 
 #define EFIAPI __attribute__((ms_abi))
-#define EFI_SUCCESS 0
-
 typedef void* EFI_HANDLE;
 typedef uint64_t EFI_STATUS;
 typedef uint16_t CHAR16;
+#define EFI_SUCCESS 0
 
-// --- STRUCTURES DE BASE ---
-typedef struct { uint16_t ScanCode; CHAR16 UnicodeChar; } EFI_INPUT_KEY;
-
-typedef struct _EFI_SIMPLE_TEXT_INPUT_PROTOCOL {
-    void* Reset;
-    EFI_STATUS (EFIAPI *ReadKeyStroke)(struct _EFI_SIMPLE_TEXT_INPUT_PROTOCOL *This, EFI_INPUT_KEY *Key);
-    void* WaitForKey;
-} EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
-
+// --- TEXT OUT ---
 typedef struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
     void* Reset;
     EFI_STATUS (EFIAPI *OutputString)(struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, const CHAR16 *String);
 } EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
-// --- GRAPHISMES (GOP) ---
-typedef enum { EfiBltVideoFill } EFI_GRAPHICS_OUTPUT_BLT_OPERATION;
-typedef struct { uint8_t B; uint8_t G; uint8_t R; uint8_t Reserved; } EFI_GRAPHICS_OUTPUT_BLT_PIXEL;
-
-typedef struct {
-    uint32_t Version; uint32_t HorizontalResolution; uint32_t VerticalResolution;
-    uint32_t PixelFormat; uint32_t PixelInformation[4]; uint32_t PixelsPerScanLine;
-} EFI_GRAPHICS_OUTPUT_MODE_INFORMATION;
-
-typedef struct {
-    uint32_t MaxMode; uint32_t Mode;
-    EFI_GRAPHICS_OUTPUT_MODE_INFORMATION *Info;
-    uint64_t Size; uint64_t FrameBufferBase; uint64_t FrameBufferSize;
-} EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE;
-
-typedef struct _EFI_GRAPHICS_OUTPUT_PROTOCOL {
-    void* QueryMode; void* SetMode;
-    EFI_STATUS (EFIAPI *Blt)(struct _EFI_GRAPHICS_OUTPUT_PROTOCOL *This, EFI_GRAPHICS_OUTPUT_BLT_PIXEL *Buffer, EFI_GRAPHICS_OUTPUT_BLT_OPERATION Operation, uint64_t SX, uint64_t SY, uint64_t DX, uint64_t DY, uint64_t W, uint64_t H, uint64_t Delta);
-    EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE *Mode;
-} EFI_GRAPHICS_OUTPUT_PROTOCOL;
-
-// --- TABLES ET SERVICES ---
-typedef struct { uint8_t Guid[16]; void *VendorTable; } EFI_CONFIGURATION_TABLE;
-
+// --- BOOT SERVICES ---
 typedef struct {
     char Sig[8]; uint32_t Rev; uint32_t Hdr; uint32_t CRC; uint32_t Res;
-    void* _p1[2];
-    EFI_STATUS (EFIAPI *AllocatePages)(int, int, uint64_t, void**);
-    void* _p2[2];
-    EFI_STATUS (EFIAPI *GetMemoryMap)(uint64_t*, void*, uint64_t*, uint64_t*, uint32_t*);
-    void* _p3[7];
-    EFI_STATUS (EFIAPI *WaitForEvent)(uint32_t, void**, uint64_t*);
-    void* _p4[12];
-    EFI_STATUS (EFIAPI *ExitBootServices)(EFI_HANDLE, uint64_t);
-    void* _p5[10];
-    EFI_STATUS (EFIAPI *LocateProtocol)(void*, void*, void**);
+    void* _p1[16]; // On saute les fonctions inutiles pour simplifier
+    EFI_STATUS (EFIAPI *WaitForEvent)(uint32_t Num, void **Event, uint64_t *Index);
+    void* _p2[13];
+    EFI_STATUS (EFIAPI *LocateProtocol)(void *Protocol, void *Reg, void **Interface);
 } EFI_BOOT_SERVICES;
 
+// --- SYSTEM TABLE ---
 typedef struct {
     char Sig[8]; uint32_t Rev; uint32_t Hdr; uint32_t CRC; uint32_t Res;
     EFI_HANDLE ConsoleInHandle;
-    EFI_SIMPLE_TEXT_INPUT_PROTOCOL *ConIn;
+    void* ConIn; // Simplifié en void* pour éviter les erreurs de structure
     EFI_HANDLE ConsoleOutHandle;
     EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut;
     void* RuntimeServices;
     EFI_BOOT_SERVICES *BootServices;
     uint64_t NumberOfTableEntries;
-    EFI_CONFIGURATION_TABLE *ConfigurationTable;
+    void* ConfigurationTable;
 } EFI_SYSTEM_TABLE;
 
 #endif
